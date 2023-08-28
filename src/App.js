375b82c516b66f881966b8a645a2811a4edf51fd
Harkirats043 <Harkirats043@gmail.com>
2023-08-28 19:11:09 -0400

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+//  Import components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {Route, Routes} from "react-router-dom";
+
+// Import Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from './pages/Projects';
+// console.log(Projects)
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React Portfolio
-        </p>
-      </header>
+      <Header />
+      <Routes>
+      <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
