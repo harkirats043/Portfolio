ef71021c92575ae935dbb18079adb14798d7ff60
Harkirats043 <Harkirats043@gmail.com>
2023-08-29 16:48:14 -0400

    testing deploy data

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,7 +12,7 @@ function About() {
 
 		// turn response into javascript object
     const data = await response.json();
-
+   console.log(data)
 		// set the about state to the data
     setAbout(data);
   };
