cd4e9c83a447b8281b96e9a5366b44bf73add709
Harkirats043 <Harkirats043@gmail.com>
2023-08-31 19:24:29 -0400

    Overall update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,8 @@ function Home(props) {
   return (
    
       <div className="homepage-content">
-        <h1 className="typewriter">Hello! I am a software developer</h1>
-      </div>
+        <h1 className="typewriter">Hello World! I am a Software Engineer</h1>
+     </div>
   
   )
 }
